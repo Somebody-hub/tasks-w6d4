d69e9ac7d890919d3689c88ca5c09253375cea30
Somebody-hub <v.kazancev17@gmai.com>
2026-09-02 13:21:00 +0500

    Initial

--- src/comparator/TaskPriorityComparator.java
+++ src/comparator/TaskPriorityComparator.java
@@ -1,12 +1,25 @@
 package comparator;
 
 import model.Task;
+import model.TaskStatus;
 
 import java.util.Comparator;
 
 public class TaskPriorityComparator implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
-        return Integer.compare(o1.getPriority(), o2.getPriority());
+        boolean firstIsDone = (o1.getTaskStatus() == TaskStatus.DONE);
+        boolean secondIsDone = (o2.getTaskStatus() == TaskStatus.DONE);
+        if (firstIsDone && !secondIsDone) {
+            return 1;
+        }
+        if (!firstIsDone && secondIsDone) {
+            return -1;
+        }
+        int priorityCompare = Integer.compare(o1.getPriority(), o2.getPriority());
+        if (priorityCompare != 0) {
+            return priorityCompare;
+        }
+        return o1.getTitle().compareTo(o2.getTitle());
     }
 }
